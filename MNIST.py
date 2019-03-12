b6009cb11fd7e1444478dbe699694091ebc1c193
hosh0425 <hosh0425@gmail.com>
2019-03-12 06:26:12 +0330

    first part of mnist

---
+++ MNIST.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 05:32:26 2019
+
+@author: Hossein
+"""
+
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense , Activation
+from keras.optimizers import SGD
+from keras.losses import categorical_crossentropy
+from keras.utils import np_utils
+import numpy as np
+
+np.random.seed(1671)
+
+NB_EPOCH = 200
+BATCH_SIZE = 128 
+VERBOSE = 1
+NB_OUTPUT = 10
+OPTIMIZER = SGD()
+NB_HIDDEN = 128
+VALIDATION_SPLIT = 0.2
+
+"""
+load dataset
+reshape
+change type
+normalize
+transform categorical features into numerical vectors (One Hot Encoding)
+"""
+(X_train , Y_train) , (X_test , Y_test) = mnist.load_data()
+
+X_train = X_train.reshape(X_train.shape[0],X_train.shape[1]*X_train.shape[2])
+X_test = X_test.reshape(X_test.shape[0] , X_test.shape[1]*X_test.shape[2])
+
+X_train = X_train.astype("float32")
+X_test = X_test.astype("float32")
+
+X_train /= 255
+X_test /= 255
+
+Y_train = np_utils.to_categorical(Y_train)
+Y_test = np_utils.to_categorical(Y_test)
+
+"""
+creating model & compile it
+"""
+
+model = Sequential()
+model.add(Dense(NB_OUTPUT , input_shape=(X_train.shape[1],)))
+model.add(Activation('softmax'))
+model.summary()
+
+"""
+common choices for matrix
+Accuracy: This is the proportion of correct predictions with respect to the targets
+Precision: This denotes how many selected items are relevant for a multilabel classification
+Recall: This denotes how many selected items are relevant for a multilabel classification
+"""
+model.compile(OPTIMIZER , loss=categorical_crossentropy , metrics=['accuracy'])
+
+
+"""
+train model
+
+epochs: This is the number of times the model is exposed to the training set. At each iteration, the
+        optimizer tries to adjust the weights so that the objective function is minimized.
+        in other words, One Epoch is when an ENTIRE dataset is passed forward and backward through the neural network only ONCE.
+        Since one epoch is too big to feed to the computer at once we divide it in several smaller batches
+
+batch_size: This is the number of training instances observed before the optimizer performs a
+            weight update. in other words: Since one epoch is too big to feed to the computer at once we divide it in several smaller batches
+            you can’t pass the entire dataset into the neural net at once. So, you divide dataset into Number of Batches or sets or part
+
+iteration: Iterations is the number of batches needed to complete one epoch.
+            The number of batches is equal to number of iterations for one epoch    
+            Let’s say we have 2000 training examples that we are going to use .
+            We can divide the dataset of 2000 examples into batches of 500 then it will take 4 iterations to complete 1 epoch
+
+We reserved part of the training set for validation. The key idea is that we reserve a
+    part of the training data for measuring the performance on the validation while
+    training. This is a good practice to follow for any machine learning task, which we
+    will adopt in all our examples.
+"""
+    history = model.fit(X_train , Y_train,
+                        batch_size = BATCH_SIZE , epochs= NB_EPOCH,
+                        verbose = VERBOSE , validation_split=VALIDATION_SPLIT)
+    
+
+"""
+test the model
+
+Note that the training set and the test set are, of course, rigorously separated. There is no point in
+    evaluating a model on an example that has already been used for training. Learning is essentially a
+    process intended to generalize unseen observations and not to memorize what is already known
+"""
+score = model.evaluate(X_test , Y_test , verbose=VERBOSE)
+print("Test score: " , score[0])
+print("Test accuracy: " , score[1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
